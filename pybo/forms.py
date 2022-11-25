1ed0507e0c5ccd9ffe9e8b328f24147f366ab6b3
CHOIisaac <ehdrhkdls@gmail.com>
2022-11-25 23:12:16 +0900

    study 11

--- pybo/forms.py
+++ pybo/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from pybo.models import Question, Answer
+from pybo.models import Question, Answer, Comment
 
 
 class QuestionForm(forms.ModelForm):
@@ -13,9 +13,19 @@ class QuestionForm(forms.ModelForm):
             'content': '내용',
         }
 
+
 class AnswerForm(forms.ModelForm):
     class Meta:
         model = Answer
+        fields = ['content']
+        labels = {
+            'content': '답변내용',
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
         fields = ['content']
         labels = {
             'content': '답변내용',
--- pybo/urls.py
+++ pybo/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import base_views, question_views, answer_views
+from .views import base_views, question_views, answer_views, comment_views
 
 app_name = 'pybo'
 
@@ -11,19 +11,23 @@ urlpatterns = [
 
     # question_views.py
     path('question/create/',
-         question_views.question_create, name='question_create'),
+        question_views.question_create, name='question_create'),
     path('question/modify/<int:question_id>/',
-         question_views.question_modify, name='question_modify'),
+        question_views.question_modify, name='question_modify'),
     path('question/delete/<int:question_id>/',
-         question_views.question_delete, name='question_delete'),
+        question_views.question_delete, name='question_delete'),
 
     # answer_views.py
     path('answer/create/<int:question_id>/',
-         answer_views.answer_create, name='answer_create'),
+        answer_views.answer_create, name='answer_create'),
     path('answer/modify/<int:answer_id>/',
-         answer_views.answer_modify, name='answer_modify'),
+        answer_views.answer_modify, name='answer_modify'),
     path('answer/delete/<int:answer_id>/',
-         answer_views.answer_delete, name='answer_delete'),
+        answer_views.answer_delete, name='answer_delete'),
     path('question/vote/<int:question_id>/', question_views.question_vote, name='question_vote'),
     path('answer/vote/<int:answer_id>/', answer_views.answer_vote, name='answer_vote'),
+
+    path('comment/create/<int:answer_id>/', comment_views.comment_create, name='comment_create'),
+    path('comment/modify/<int:comment_id>/', comment_views.comment_modify, name='comment_modify'),
+    path('comment/delete/<int:comment_id>/', comment_views.comment_delete, name='comment_delete'),
 ]
